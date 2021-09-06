854dc182aad2922d7f2bd835d77dad64e14d126d
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-09-07 01:01:00 +0200

    Create layout.js

---
+++ src/components/layout.js
@@ -0,0 +1,68 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+ import React from "react"
+ import PropTypes from "prop-types"
+ import { useStaticQuery, graphql } from "gatsby"
+ import { IonApp, IonPage, IonContent, IonFooter, IonToolbar, IonTitle } from '@ionic/react'
+ 
+ import Header from "./header"
+ import "./layout.css"
+ 
+ /* Core CSS required for Ionic components to work properly */
+ import '@ionic/react/css/core.css'
+ 
+ /* Basic CSS for apps built with Ionic */
+ import '@ionic/react/css/normalize.css'
+ import '@ionic/react/css/structure.css'
+ import '@ionic/react/css/typography.css'
+ 
+ /* Optional CSS utils that can be commented out */
+ import '@ionic/react/css/padding.css'
+ import '@ionic/react/css/float-elements.css'
+ import '@ionic/react/css/text-alignment.css'
+ import '@ionic/react/css/text-transformation.css'
+ import '@ionic/react/css/flex-utils.css'
+ import '@ionic/react/css/display.css'
+ 
+ const Layout = ({ children }) => {
+   const data = useStaticQuery(graphql`
+     query SiteTitleQuery {
+       site {
+         siteMetadata {
+           title
+         }
+       }
+     }
+   `)
+ 
+   return (
+     <IonApp>
+       <IonPage>
+         <Header siteTitle={data.site.siteMetadata.title} />
+         <IonContent className="ion-padding">
+           <main>{children}</main>
+         </IonContent>
+         <IonFooter>
+           <IonToolbar>
+             <IonTitle>
+               © {new Date().getFullYear()}, Built with
+               {` `}
+               <a href="https://www.gatsbyjs.org">Gatsby</a>
+             </IonTitle>
+           </IonToolbar>
+         </IonFooter>
+       </IonPage>
+     </IonApp>
+   )
+ }
+ 
+ Layout.propTypes = {
+   children: PropTypes.node.isRequired,
+ }
+ 
+ export default Layout
